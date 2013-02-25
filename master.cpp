7fdf03b0459e90fa69e4b6c837524c35a5ad7429
santiago <santiago@santiago-Latitude-E6420.(none)>
2013-02-24 19:58:04 -0600

    test commit

--- master.cpp
+++ master.cpp
@@ -19,17 +19,58 @@ void printFloatVector( vector<float>&);
 **************************************************************************************************/
 int main( int argc, char** argv)
 {
+	const int NBUFFERS = 1;
+	const int NWORKERS = 2;
+	const int SLEEPMIN = 3;
+	const int SLEEPMAX = 4;
+
+	int i,r;
+	int nBuffers, nWorkers;
+	float sleepMin, sleepMax;
+	int randSeed = time( 0);
+	bool lck = 0;
 	
+	const char* errcstr = "Bad parameters\n";
 
+	if( argc < 5 || argc>7) finish( errcstr,1,0);
 
-	/*
+	//if( (nBuffers = atoi( argv[NBUFFERS])) <=0)
+	if( !(r=sscanf(argv[NBUFFERS],"%d",&nBuffers)) || r==EOF)
+		finish( errcstr,1,0);
+	if( !(r=sscanf(argv[NWORKERS],"%d",&nWorkers)) || r==EOF || nWorkers>=nBuffers || nWorkers<=0)
+		finish( errcstr,1,0);
+	if( !(r=sscanf( argv[SLEEPMIN], "%f", &sleepMin)) || r==EOF)
+		finish( errcstr,1,0);
+	if( !(r=sscanf( argv[SLEEPMAX], "%f", &sleepMax)) || r==EOF || sleepMax<=sleepMin)
+		finish( errcstr,1,0);
+	
+	char* adarg[2] = {argc>5?argv[5]:0, argc>6?argv[6]:0};
+	
+	for( i=0; i<2; i++) if( adarg[i])
+	{
+		if( string(adarg[i])=="-lock")
+			lck = 1;
+		else if( string(adarg[i]) == "-nolock")
+			lck = 0;
+		else if( !(r=sscanf( adarg[i], "%d", &randSeed)) || r==EOF)
+			finish( errcstr,1,0);
+	}
+
+	//* PRINT PARAMETERS
+	printf("nBuffers[%d]\n", nBuffers); 
+	printf("nWorkers[%d]\n", nWorkers); 
+	printf("sleepMin[%.2f]\n",sleepMin);
+	printf("sleepMax[%.2f]\n", sleepMax);
+	printf("randSeed[%d]\n", randSeed);
+	printf("lck[%d]\n", lck);
+	//*/
+
 	srand( time(0));
 	vector<float> res = getRandoms( 10, 0, 100);
 	
 	printFloatVector( res);	
 	sortRandoms( res);
 	printFloatVector( res);
-	//*/
 
 	return 0;
 }
